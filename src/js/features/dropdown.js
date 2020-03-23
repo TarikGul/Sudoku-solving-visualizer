af1f2a4e2903b79832b45f3c5bc74046618ff091
Tarik <tariksnow37@gmail.com>
2020-03-23 00:46:36 -0700

    add dropdown

--- src/js/features/dropdown.js
+++ src/js/features/dropdown.js
@@ -1,4 +1,4 @@
-let x, i, j, selElmnt, a, b, c;
+var x, i, j, selElmnt, a, b, c;
 /* Look for any elements with the class "custom-select": */
 x = document.getElementsByClassName("custom-select");
 for (i = 0; i < x.length; i++) {
@@ -19,7 +19,7 @@ for (i = 0; i < x.length; i++) {
         c.addEventListener("click", function (e) {
             /* When an item is clicked, update the original select box,
             and the selected item: */
-            let y, i, k, s, h;
+            var y, i, k, s, h;
             s = this.parentNode.parentNode.getElementsByTagName("select")[0];
             h = this.parentNode.previousSibling;
             for (i = 0; i < s.length; i++) {
@@ -52,7 +52,7 @@ for (i = 0; i < x.length; i++) {
 function closeAllSelect(elmnt) {
     /* A function that will close all select boxes in the document,
     except the current select box: */
-    let x, y, i, arrNo = [];
+    var x, y, i, arrNo = [];
     x = document.getElementsByClassName("select-items");
     y = document.getElementsByClassName("select-selected");
     for (i = 0; i < y.length; i++) {
