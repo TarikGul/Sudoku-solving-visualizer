6d3d1c629450e9020abf86a4769b0a9479d0c8e8
Tarik <tariksnow37@gmail.com>
2020-03-27 12:07:54 -0700

    add time html

--- src/js/sudoku/visualize.js
+++ src/js/sudoku/visualize.js
@@ -5,7 +5,7 @@ import _ from 'lodash'
 // Once a person selects and clicks solve it will visualize the algorithm for you
 
 class Visualize {
-    constructor(algorithm, board, speed = 25) {
+    constructor(algorithm, board, speed = 100) {
         this.algorithm = algorithm
         this.board = board
         this.orderedTraversal;
@@ -13,6 +13,7 @@ class Visualize {
         this.reset = false;
         this.speed = speed
         this.count = 0;
+        this.time = 0
     }
 
     // This is where we choose which algorithm is going to be solved an initialized
